28683b530e0c8124998f6fe3b2f939ca69e685e8
Kirichenko-sanek <alexander.kirichenkoo@gmail.com>
2016-11-07 13:13:10 +0200

    Fix bugs

--- Crawler.BL/Manager/ActivComManager.cs
+++ Crawler.BL/Manager/ActivComManager.cs
@@ -48,158 +48,129 @@ namespace Crawler.BL.Manager
         public List<string> GetListPages(string type)
         {
 
-            var wClient = new WebClient();
-            var html = new HtmlDocument();
-            var elementList = new List<string>();           
-            for (int i = 1; ; i++)
+            var elementList = new List<string>();
+            int cout = 1;
+            var wClientStart = new WebClient();
+            var htmlStart = new HtmlDocument();
+            htmlStart.LoadHtml(wClientStart.DownloadString("http://www.active.com/" + type + "?radius=infinity" + "&page=" + 1));
+
+            var pages = htmlStart.DocumentNode.SelectNodes("//div[@id='paging']/ul/li");
+            if (pages != null)
             {
-                html.LoadHtml(wClient.DownloadString("http://www.active.com/" + type + "?radius=infinity" + "&page=" + i));
-                var elements = html.DocumentNode.SelectNodes("//a[@data-asset-id]");
-                if (elements == null)
-                {
-                    break;
-                }
-                foreach (var item in elements)
-                {
-                    elementList.Add("http://www.active.com/" + item.GetAttributeValue("href", "") +
-                                    item.GetAttributeValue("data-asset-id", ""));
-                }
+                cout = Convert.ToInt32((pages[pages.Count - 1]).InnerText);
             }
+            elementList.Clear();
 
-            //var elementList = new List<string>();
-            //int cout = 1;
-            //var wClientStart = new WebClient();
-            //var htmlStart = new HtmlDocument();
-            //htmlStart.LoadHtml(wClientStart.DownloadString("http://www.active.com/" + type + "?radius=infinity" + "&page=" + 1));
-            //try
-            //{
-            //    var pages = htmlStart.DocumentNode.SelectNodes("//div[@id='paging']/ul/li");
-            //    if (pages != null)
-            //    {
-            //        cout = Convert.ToInt32((pages[pages.Count - 1]).InnerText);
-            //    }
-            //    elementList.Clear();
+            Parallel.For(1, cout, i =>
+            {
+                using (var wClient = new WebClient())
+                {
+                    var html = new HtmlDocument();
+                    try
+                    {
+                        html.LoadHtml(
+                            wClient.DownloadString("http://www.active.com/" + type + "?radius=infinity" + "&page=" + i));
+                    }
+                    catch (Exception)
+                    {
+                        return;
+                    }
 
-            //    Parallel.For(1, cout, i =>
-            //    {
-            //        var wClient = new WebClient();
-            //        var html = new HtmlDocument();
-            //        html.LoadHtml(wClient.DownloadString("http://www.active.com/" + type + "?radius=infinity" + "&page=" + i));
-            //        var elements = html.DocumentNode.SelectNodes("//a[@data-asset-id]");
-            //        if (elements == null)
-            //        {
-            //            return;
-            //        }
-            //        foreach (var item in elements)
-            //        {
-            //            elementList.Add("http://www.active.com/" + item.GetAttributeValue("href", "") +
-            //                            item.GetAttributeValue("data-asset-id", ""));
-            //        }
+                    var elements = html.DocumentNode.SelectNodes("//a[@data-asset-id]");
+                    if (elements == null)
+                    {
+                        return;
+                    }
+                    foreach (var item in elements)
+                    {
+                        elementList.Add("http://www.active.com/" + item.GetAttributeValue("href", ""));
+                    }
+                }
+            });
 
-            //    });
-            //}
-            //catch (Exception)
-            //{
-            //    var wClient = new WebClient();
-            //    var html = new HtmlDocument();
-            //    elementList.Clear();
-            //    for (int i = 1; ; i++)
-            //    {
-            //        html.LoadHtml(wClient.DownloadString("http://www.active.com/" + type + "?radius=infinity" + "&page=" + i));
-            //        var elements = html.DocumentNode.SelectNodes("//a[@data-asset-id]");
-            //        if (elements == null)
-            //        {
-            //            break;
-            //        }
-            //        foreach (var item in elements)
-            //        {
-            //            elementList.Add("http://www.active.com/" + item.GetAttributeValue("href", "") +
-            //                            item.GetAttributeValue("data-asset-id", ""));
-            //        }
-            //    }
-            //}
-            
             return elementList;
         }
 
         public List<Info> ParsePages(List<string> elementList, string type)
-        {      
+        {
             var info = new List<Info>();
 
             Parallel.ForEach(elementList, item =>
             {
-                var wClient = new WebClient();
-                var htmlInfo = new HtmlDocument();
-                var tagsList = new List<string>();
-                var information = new Info();
-                try
+                using (var wClient = new WebClient())
                 {
-                    htmlInfo.LoadHtml(wClient.DownloadString(item.Replace("http://www.activekids.com/", "")));
-                }
-                catch (Exception)
-                {
-                    return;
-                }
-
-                information.Name =
-                    htmlInfo.DocumentNode.SelectSingleNode("//div[@class='ed-details']/h1")?
-                        .InnerText.Replace("&amp;", "and").Replace(";", "and") ?? " ";
-                Console.WriteLine("Information about the event collection:" + information.Name);
-
-                information.Date =
-                    htmlInfo.DocumentNode.SelectSingleNode(
-                        "//div[@class='ed-details']/div[@class='visible-desktop']/h5")?
-                        .InnerText.Replace(";", "and") ?? " ";
-
-                try
-                {
-                    var adressName = htmlInfo.DocumentNode.SelectSingleNode(
-                        "//div[@class='ed-details']/div[@class='visible-desktop']/div[@class='event-details-address']/span[@class='ed-address-name']")
-                        ?.InnerText.Trim().Replace(";", "and") ?? " ";
-                    var adress =
-                        htmlInfo.DocumentNode.SelectNodes(
-                            "//div[@class='ed-details']/div[@class='visible-desktop']/div[@class='event-details-address']/span[@class='ed-address-text']/span");
-                    information.Place = adressName.Replace("&nbsp;", "").Replace("&amp;", "and") + " ";
-                    foreach (var str in adress)
+                    var htmlInfo = new HtmlDocument();
+                    var tagsList = new List<string>();
+                    var information = new Info();
+                    try
                     {
-                        information.Place = information.Place +
-                                            str.InnerText.Replace("&nbsp;", "")
-                                                .Replace("&amp;", "and")
-                                                .Replace(";", "and") + " ";
+                        htmlInfo.LoadHtml(wClient.DownloadString(item.Replace("http://www.activekids.com/", "")));
                     }
-                }
-                catch (Exception)
-                {
-                    information.Place = " ";
-                }
-
-                try
-                {
-                    var tags = htmlInfo.DocumentNode.SelectNodes("//ul[@class='inline breadcrumb']/li/p/a");
-                    foreach (var tag in tags)
+                    catch (Exception)
                     {
-                        tagsList.Add(tag.InnerText);
+                        return;
                     }
 
+                    information.Name =
+                        htmlInfo.DocumentNode.SelectSingleNode("//div[@class='ed-details']/h1")?
+                            .InnerText.Replace("&amp;", "and").Replace(";", "and") ?? " ";
+                    Console.WriteLine("Information about the event collection:" + information.Name);
+
+                    information.Date =
+                        htmlInfo.DocumentNode.SelectSingleNode(
+                            "//div[@class='ed-details']/div[@class='visible-desktop']/h5")?
+                            .InnerText.Replace(";", "and") ?? " ";
+
+                    try
+                    {
+                        var adressName = htmlInfo.DocumentNode.SelectSingleNode(
+                            "//div[@class='ed-details']/div[@class='visible-desktop']/div[@class='event-details-address']/span[@class='ed-address-name']")
+                            ?.InnerText.Trim().Replace(";", "and") ?? " ";
+                        var adress =
+                            htmlInfo.DocumentNode.SelectNodes(
+                                "//div[@class='ed-details']/div[@class='visible-desktop']/div[@class='event-details-address']/span[@class='ed-address-text']/span");
+                        information.Place = adressName.Replace("&nbsp;", "").Replace("&amp;", "and") + " ";
+                        foreach (var str in adress)
+                        {
+                            information.Place = information.Place +
+                                                str.InnerText.Replace("&nbsp;", "")
+                                                    .Replace("&amp;", "and")
+                                                    .Replace(";", "and") + " ";
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        information.Place = " ";
+                    }
+
+                    try
+                    {
+                        var tags = htmlInfo.DocumentNode.SelectNodes("//ul[@class='inline breadcrumb']/li/p/a");
+                        foreach (var tag in tags)
+                        {
+                            tagsList.Add(tag.InnerText);
+                        }
+
+                    }
+                    catch (Exception)
+                    {
+                        tagsList.Add(" ");
+                    }
+                    information.Tag = tagsList;
+
+                    information.OrganizerName =
+                        htmlInfo.DocumentNode.SelectSingleNode("//div[@class='sectioncontent']/ul/li/h5")?
+                            .InnerText.Replace("&amp;", "and")
+                            .Replace(";", "and") ?? " ";
+
+                    information.Organizer =
+                        htmlInfo.DocumentNode.SelectSingleNode("//div[@class='sectioncontent']/ul/li/p/a")?
+                            .GetAttributeValue("href", "").Replace(";", "and") ?? " ";
+
+                    information.Url = item;
+                    information.Category = type;
+                    info.Add(information);
                 }
-                catch (Exception)
-                {
-                    tagsList.Add(" ");
-                }
-                information.Tag = tagsList;
-
-                information.OrganizerName =
-                    htmlInfo.DocumentNode.SelectSingleNode("//div[@class='sectioncontent']/ul/li/h5")?
-                        .InnerText.Replace("&amp;", "and")
-                        .Replace(";", "and") ?? " ";
-
-                information.Organizer =
-                    htmlInfo.DocumentNode.SelectSingleNode("//div[@class='sectioncontent']/ul/li/p/a")?
-                        .GetAttributeValue("href", "").Replace(";", "and") ?? " ";
-
-                information.Url = item;
-                information.Category = type;
-                info.Add(information);
             });
 
             return info;
